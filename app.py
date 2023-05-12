a39da4897670fe9d54ea453c1d1578e5e5bd1ed8
Lorenzo Jiang <lorenzo.jiang8075@gmail.com>
2023-05-11 23:05:49 -0700

    added action to form

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def index():
 
     return render_template("index.html",css=url_for('static',filename='styles.css'))
 
-@app.route("/thankyou", methods=["GET"])
+@app.route("/thankyou", methods=["GET", "POST"])
 def thank_you():
     return render_template("thankyou.html")
 
